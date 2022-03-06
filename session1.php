50034523e176dc2a5ea0e9916c2d57c74bc497a6
nadeen yehia <47985921+nadeenyehia@users.noreply.github.com>
2022-03-07 00:25:54 +0200

    Add files via upload

---
+++ session1.php
@@ -0,0 +1,31 @@
+<?php
+$result_str = $result = '';
+if (isset($_POST['unit-submit'])) {
+    $units = $_POST['units'];
+    if (!empty($units)) {
+        $result = calculate_bill($units);
+        $result_str = 'Total amount of ' . $units . ' - ' . $result;
+    }
+}
+    $unit_cost_first = 3.50;
+    $unit_cost_second = 4.00;
+    $unit_cost_third = 6.50;
+
+    if($units <= 50) {
+        $bill = $units * $unit_cost_first;
+    }
+    else if($units > 50 && $units <= 100) {
+        $temp = 50 * $unit_cost_first;
+        $remaining_units = $units - 50;
+        $bill = $temp + ($remaining_units * $unit_cost_second);
+    }
+    else if($units > 150) {
+        $bill = $units * $unit_cost_third;
+    }
+    else {
+        $temp = (50 * 3.5) + (100 * $unit_cost_second) + (100 * $unit_cost_third);
+        $remaining_units = $units - 250;
+        $bill = $temp + ($remaining_units * $unit_cost_fourth);
+    }
+
+?>
